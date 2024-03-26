9cd9ce48ba5920c5652c462191553f1200706d41
星曜 <yzxxy010@163.com>
2024-03-26 18:32:19 +0800

    添加项目文件。

---
+++ ChoasWorld.cs
@@ -0,0 +1,12 @@
+﻿using NeoModLoader.api;
+
+namespace ChoasWorld
+{
+    internal class ChoasWorld : BasicMod<ChoasWorld>
+    {
+        protected override void OnModLoad()
+        {
+            LogInfo("mod测试");
+        }
+    }
+}
