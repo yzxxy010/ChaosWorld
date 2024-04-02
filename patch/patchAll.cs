31ee7063e313d0d3ad4dbe255fae6d12adc24720
星曜 <yzxxy010@163.com>
2024-04-03 05:47:19 +0800

    去除了actor的无敌帧

---
+++ patch/patchAll.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace ChaosWorld.patch
+{
+    internal class patchAll
+    {
+        public static void Init()
+        {
+            Harmony harmony = new Harmony("xingyao.mod.worldbox.chaosWorld");
+            harmony.PatchAll(typeof(removeInvincibleFrames));
+        }
+    }
+}
---
+++ patch/removeInvincibleFrames.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+namespace ChaosWorld.patch
+{
+    internal class removeInvincibleFrames
+    {
+        [HarmonyPrefix, HarmonyPatch(typeof(Actor), "getHit")]
+        public static bool Prefix(float pDamage, ref bool pFlash, AttackType pAttackType, ref BaseSimObject pAttacker, bool pSkipIfShake, bool pMetallicWeapon)
+        {
+            pSkipIfShake = false;
+            return true;
+        }
+    }
+}
