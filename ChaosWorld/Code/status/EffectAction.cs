6bd5a27bc6dff6f685b85463986714413f7bfbae
星曜 <yzxxy010@163.com>
2024-06-14 18:32:25 +0800

    优化debug流程,只有进入MOD_DEBUG_MODE状态,才会触发debuglog

--- ChaosWorld/Code/status/EffectAction.cs
+++ ChaosWorld/Code/status/EffectAction.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
-
+using ChaosWorld_Debug;
 using ReflectionUtility;
 
 using UnityEngine;
@@ -15,7 +15,7 @@ namespace ChaosWorld.status
     {
         public static bool heavyCurse_WorldAction(BaseSimObject pTarget, WorldTile pTile)
         {
-            Debug.Log(pTarget.a.getName());
+            DEBUG_MODE.instance.Log(pTarget.a.getName());
             if (pTarget != null)
             {
                 Actor actor = pTarget.a;
